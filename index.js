55c35915a1207084e6f3e7a05e32703bc84decd9
Aakash Bhandari <akashbhandari930@gmail.com>
2021-12-13 20:33:39 -0800

    fixing errors

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Dependencies 
-const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
